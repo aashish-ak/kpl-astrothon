57525a94279e8290feb36c9e9f88f5fddc44f484
Shrikha <mahanty.shrikha@gmail.com>
2020-06-08 18:41:40 +0530

    add question_2 function

--- q2/main.py
+++ q2/main.py
@@ -1,18 +1,19 @@
 # all imports below
-
+import math as m
 """
 Any extra lines of code (if required)
 as helper for this function.
 """
 
 def findDistance(vrec):
-	'''
-	Parameters
-	----------
-	vrec : a `float`
-	
-	Returns
-	-------
-	a `float`
-	'''
-	return NotImplementedError
+    '''
+    Parameters
+    ----------
+    vrec : a `float`
+    
+    Returns
+    -------
+    a `float`
+    '''
+    a = (vrec/(71))*3.26*(m.pow(10,6))
+    return a

a609c8a0326a2df85c8a1aa615f36c8202c34121
aashish-ak <kumaraashish118@gmail.com>
2020-06-08 19:16:58 +0530

    Fetched main page

--- q5/main.py
+++ q5/main.py
@@ -1,11 +1,7 @@
 # all imports below
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
-
-URL = 'http://solar.physics.montana.edu/HINODE/XRT/QL/syn_comp_fits/'
-pageResponse = requests.get(URL)
-
-bsParser = BeautifulSoup(pageResponse.content, 'html.parser')
 
 """
 Any extra lines of code (if required)
@@ -20,14 +16,20 @@ class ScraperXRT:
     '''
 
     def __init__(self, typeof_file, startime, endtime):
-    '''
-    Parameters
-	----------
-	typeof_file: A `string`
-    startime: A `~datetime.datetime` instance
-    endtime: A `~datetime.datetime` instance
-	'''
-
+      # start = datetime.strptime('20180601', '%Y%m%d').date()
+      # end = datetime.strptime('20180601', '%Y%m%d').date()
+      start = startime.strftime("%Y%m%d_%H%M%S")
+      end = endtime.strftime("%Y%m%d_%H%M%S")
+      print(start, end)
+      URL = 'http://solar.physics.montana.edu/HINODE/XRT/QL/syn_comp_fits/'
+      pageResponse = requests.get(URL)
+      bsParser = BeautifulSoup(pageResponse.content, 'html.parser')
+      
+      # File number 5 - 4989 contains links that we want.
+      
+      files = bsParser.find_all('a') 
+      print(files[4989])
+      
     def query(self):
 	'''
 	Returns
@@ -55,3 +57,7 @@ class ScraperXRT:
 	An instance of `matplotlib.image.AxesImage`, returned using `plt.imshow(data)`.
 	'''
 	return NotImplementedError
+
+
+if __name__ == "__main__":
+    scraper = ScraperXRT("asd", datetime(2018, 1, 1), datetime.now())

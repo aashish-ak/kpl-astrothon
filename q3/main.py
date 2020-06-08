ae25fa11331e14c85979c5ed3464492f34f1c003
Aryan Garg <54898594+Aryan-Garg@users.noreply.github.com>
2020-06-08 19:19:33 +0530

    Update main.py

--- q3/main.py
+++ q3/main.py
@@ -4,8 +4,10 @@
 Any extra lines of code (if required)
 as helper for this function.
 
-Gravitational Time Dilation = 1 - sqrt( 1 - ( 2*G*M / (dist-R)*(c**2) ) ) * (nano secs in a day)
-1/gamma = sqrt(1-2GM/R(c**2))
+Gravitational Time Dilation = sqrt( 1 - ( 2*G*M / (dist-R)*(c**2) ) ) * (nano secs in a day)
+(after approximation, we are interested in finding the diff between earth and the satellite) 
+delta(1/gamma) = GM/R(c**2) - GM/dist(c**2)
+Time dilation = nanosecs in a day * delta(1/gamma)
 """
 
 def findDelay(dist):
@@ -18,9 +20,10 @@ def findDelay(dist):
 	-------
 	A `float`
 	'''
-	twoGM_by_c_sq = (2*6.674*5.974)/(2.998*2.998*1000)
-        one_by_gamma = sqrt( 1 - twoGM_by_c_sq/(dist-R))
-        nanosecs_in_a_day = 24*60*60*(10**9)
+	R = 6357000 # in SI units
+	# Simplified to GM/c^2(1/R - 1/dist)
+	delta_gamma = ( (6.674 * 5.974) / (2.998**2) ) * ((1.0/R) - (1.0/dist))
+        nanosecs_in_a_day = 24*60*60*(10**6) # divided by 10**3 due to c^2 computation above
 	
-	return (1 - one_by_gamma)*nanosecs_in_a_day
+	return delta_gamma*nanosecs_in_a_day
 
